504baa1b2bee2bd4ff34ee966902cbcaa3e87820
mansha1 <42634181+mansha1@users.noreply.github.com>
2018-10-16 01:20:13 +0530

    Add files via upload

---
+++ dds1/Question3.java
@@ -0,0 +1,15 @@
+    public class Question1{
+        public static void main(String[] x){
+                     new Child().foo();
+               }
+             }
+     class Parent{
+          public void foo(){
+               System.out.println("I'am in the parent");
+              }   
+          }
+      class Child extends Parent{
+   Final();
+   
+    }
+}
